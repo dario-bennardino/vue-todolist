f2dcac5df8121ae4597f5ac5af61d41f81765d6d
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-03-13 16:20:43 +0100

    [add] aggiungere testo sbarrato

--- js/script.js
+++ js/script.js
@@ -20,7 +20,8 @@ createApp({
 
             // Aggiungo nell'array all'array todoList un nuovo oggetto con la proprietà 'text' e 'done'
             this.todoList.push({ text: this.newTask, todoDone: false }); 
-            this.newTask = ''; // Resettare l'input     
+            // Resettare l'input 
+            this.newTask = '';     
         }
        
         
